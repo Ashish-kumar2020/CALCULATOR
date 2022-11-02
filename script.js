290896ef7ec3b67accef31558bdb3df8721f0e83
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2022-11-02 22:57:28 +0530

    added logic for calulator

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log('calculator');
+
+let btns = document.querySelectorAll("button");
+let screen = document.getElementById("answer");
+
+let screenValue = "";
+let buttonText;
+for(item of btns){
+    item.addEventListener('click',(e)=>{
+        
+        buttonText = e.target.innerText;
+        
+         if(buttonText == "C"){
+            screenValue = "";
+            screen.value = screenValue;
+        } else if(buttonText == "="){
+            screenValue = eval(screenValue);
+            screen.value = screenValue;
+        } 
+        else{
+            screenValue = screenValue + e.target.innerHTML;
+            screen.value = screenValue
+        }
+        console.log('click',e.target.innerText);
+    })
+}
